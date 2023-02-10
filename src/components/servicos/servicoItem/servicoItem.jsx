c07e70825719e7a6842d119d8eafd65df039db13
Pedro R <rochapedro436@gmail.com>
2023-02-10 17:07:18 -0300

    Teste 3

--- src/components/servicos/servicoItem/servicoItem.jsx
+++ src/components/servicos/servicoItem/servicoItem.jsx
@@ -7,8 +7,8 @@ export default function ServicoItem({logo, titulo, descricao}) {
             <div className="ServicoItem">
                 <img src={logo}></img>
                 <h4 className="TituloServicoItem">{titulo} </h4>
-                <span>{descricao}</span>
-                
+                <p>{descricao}</p>
+               
             </div>
         </>
         
