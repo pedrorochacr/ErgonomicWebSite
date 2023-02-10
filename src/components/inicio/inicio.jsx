49e337fcf4cfd244c310a55340c1d4f8bb73e7fd
Pedro R <rochapedro436@gmail.com>
2023-02-09 23:00:14 -0300

    Ajustes imagens de inicio

--- src/components/inicio/inicio.jsx
+++ src/components/inicio/inicio.jsx
@@ -1,24 +1,79 @@
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+import fundo2 from "../../assets/6b7666ab_1.png"
+import fundo3 from "../../assets/fundo3.png"
+import fundo1 from "../../assets/fundoEmpresa.png"
 import "./inicio.css"
 export default function Inicio() {
   return (
-        <>
-          <div className="root" id='inicio'>
-                <p className="description">
-                  A importância da <br></br> <span className='orange'>Ergonomia</span> para sua empresa<br></br>
-                  <span className='description02'>
-                      <p>
-                      Garanta para a sua empresa <span className='orange'>conforto</span>, <br></br> <span className='orange'>prevenção</span> e <span className='orange'>saúde</span>.
-                      Cuide de quem cuida <br></br>dos seus <span className='orange'>clientes</span>
-                      </p>
-                  </span>
-                 
-                  
-                </p>
-            </div>
-           
-        </>
+    <>
+      <div className="root" id='inicio'>
         
+              
+        <Swiper
+          slidesPerView={1}
+          centeredSlides={true}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          
+         
+          modules={[Autoplay, Navigation]}
+          
+        >
+          <SwiperSlide>   
+              <img src={fundo1}></img>
+              <section className="description">
+                  <h4>A importância da <br></br> <span className='orange'>Ergonomia</span> para sua empresa</h4>
+                  <p className='description02'>
+                  Garanta para a sua empresa <span className='orange'>conforto</span>, <span className='orange'>prevenção</span> e <span className='orange'>saúde</span>.
+                  Cuide de quem cuida <br></br>dos seus <span className='orange'>clientes</span>
+                  </p>
+              </section>
+              <div className="fundo"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+              <img src={fundo2}></img>
+              <section className="description">
+                  <h4>Consultoria em <br></br> Home Office</h4>
+                  <p className='description02'>
+                  Oferecemos avaliações <span className="orange">detalhadas</span> do espaço de trabalho,
+                  recomendações de <span className="orange">equipamentos</span> e ajustes de <span className="orange">ergonomia</span>.
+                  </p>
+              </section>
+              <div className="fundo"></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt="designed by Freepik" src={fundo3}></img>
+              <section className="description">
+                  <h4>Serviços para <br></br> Indústria</h4>
+                  <p className='description02'>
+                    Entendemos a importância da <span className="orange">ergonomia</span> 
+                     no ambiente de trabalho <span className="orange">industrial</span> e trabalhamos 
+                     para maximizar a <span className="orange">segurança, eficiência e conforto</span> dos funcionários.
+                  </p>
+              </section>
+              <div className="fundo"></div>
+              
+          </SwiperSlide>
+        
+        </Swiper>
+      </div>
+
+    </>
+
+
+
   );
 }
 
